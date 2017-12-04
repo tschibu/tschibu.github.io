819b7165ed77059a7fbebee0b444fc9109499a3c
tcschwar <Remo.Schwarzentruber@stud.hslu.ch>
2017-12-04 11:02:23 +0100

    canvas first step

---
+++ js/canvas.js
@@ -0,0 +1,20 @@
+// Die Canvas-Funktion beim Laden der Seite aufrufen
+if(window.addEventListener){
+	addEventListener("load", drawCanvas, false);
+}
+
+
+//Canvas zeichnen
+function drawMyCanvas(){
+	var canvas = document.getElementById('myCanvasPanel');
+	var img = new Image();
+	img.onload = function(){
+		if(canvas.getContext){
+			var context = canvas.getContext('2d');
+			context.translate(200, 0);
+			context.rotate(90 * Math.PI/180);
+			context.drawImage(img, 0, 0);
+		}
+	}
+	img.src = '../img/lucerne.png';
+}
